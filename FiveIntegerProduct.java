18b02c21b033cb296df996c68733c3e89dd1c630
rafeeq196 <85739620+rafeeq196@users.noreply.github.com>
2022-09-23 23:11:06 +0530

    Create FiveIntegerProduct.java

---
+++ FiveIntegerProduct.java
@@ -0,0 +1,17 @@
+package Exercise;
+
+public class FiveIntegerProduct {
+
+	public static void main(String[] args) {
+		int number1 = 2;
+		int number2 = 5;
+		int number3 = 3;
+		int number4 = 8;
+		int number5 = 7;
+		int Product;
+		Product = number1 * number2 * number3 * number4 * number5  ;
+		
+		System.out.println("The Product is " +Product);
+	}
+
+}
