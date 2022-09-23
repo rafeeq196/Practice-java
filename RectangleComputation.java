4b01ff261343ddd7b489d27c5002cf686b9dcdc7
rafeeq196 <85739620+rafeeq196@users.noreply.github.com>
2022-09-23 23:33:49 +0530

    Create RectangleComputation.java

---
+++ RectangleComputation.java
@@ -0,0 +1,14 @@
+package Exercise;
+
+public class RectangleComputation {
+
+	public static void main(String[] args) {
+		double lenth = 2.3;
+		double width = 1.5;
+		double area = lenth*width;
+		double perimeter = 2*(lenth+width);
+		System.out.println(" The area of Rectangle is "+area);
+		System.out.println(" The area of perimeter is "+perimeter);
+	}
+
+}
