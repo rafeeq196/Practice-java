d583431a64d6c81743cc7148aaea7c2adc5791f6
rafeeq196 <85739620+rafeeq196@users.noreply.github.com>
2022-09-24 19:30:20 +0530

    Create SumProduct2Numbers.java

---
+++ SumProduct2Numbers.java
@@ -0,0 +1,18 @@
+package Exercise;
+
+public class SumProduct2Numbers {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int c = 15;
+		int x1 = 31*a + 17*b;//sum of 31a+17b
+		int x2 = 31*a + 17*b +77*c ;//sum of 5a+10b+77c
+		System.out.println("x1 is  "+x1);
+		System.out.println("x2 is "+x2);
+		
+		
+
+	}
+
+}
