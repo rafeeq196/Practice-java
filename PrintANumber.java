09096da7cf0c4b229cc5e012c3d76e9692d641a1
rafeeq196 <85739620+rafeeq196@users.noreply.github.com>
2022-09-25 22:25:25 +0530

    Create PrintANumber.java

---
+++ PrintANumber.java
@@ -0,0 +1,14 @@
+package questions27;
+
+import java.util.Scanner;
+
+public class PrintANumber {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter your choice of number");
+		int number = reader.nextInt();
+		System.out.println("The number you have entered is "+number);
+	}
+
+}
