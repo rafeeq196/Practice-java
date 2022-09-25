8325cf28802d598df7766aab5d0006567bc46f68
rafeeq196 <85739620+rafeeq196@users.noreply.github.com>
2022-09-25 22:37:35 +0530

    Create Basic2.java

---
+++ Basic2.java
@@ -0,0 +1,19 @@
+package questions27;
+
+import java.util.Scanner;
+
+public class Basic2 {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter value of i and j to multiplication");
+		System.out.println("Firt Enter value of i to multiply");
+		int i = obj.nextInt();
+		System.out.println("now Enter value of j to multiply");
+		int j = obj.nextInt();
+		System.out.println("The multiplication is "+i*j);
+		
+
+	}
+
+}
