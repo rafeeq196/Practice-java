be1220ea9fec535c43346758ac5e3ce0fce3476c
rafeeq196 <85739620+rafeeq196@users.noreply.github.com>
2022-09-25 22:07:58 +0530

    Create addTwoNumbers.java

---
+++ addTwoNumbers.java
@@ -0,0 +1,13 @@
+package questions27;
+
+public class addTwoNumbers {
+
+	public static void main(String[] args) {
+		int first = 100;
+		int second = 200;
+		int sum = first+second;
+		System.out.println("The sum of two numbers is "+sum);
+
+	}
+
+}
